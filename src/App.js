c65632ec8ad68e4836c200aaccb99618430cacf2
monomuymono <javier.moraga.correa@gmail.com>
2025-08-02 15:50:37 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1045,7 +1045,7 @@ const App = () => {
 
     return (
         <div className="min-h-screen bg-gray-50 font-sans text-gray-800">
-            <LoadingModal isOpen={isGeneratingLink} message="Generando enlace..." />
+            <LoadingModal isOpen={isGeneratingLink} message={loadingMessage} />
             <div className="max-w-4xl mx-auto p-4">{renderStep()}</div>
             <div style={{ display: 'none' }}>
                 <div id="print-source-content">
